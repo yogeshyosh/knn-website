78b8a1e9ddf69d889449b9833b027ebdbd40b878
yogeshyosh <122715072+yogeshyosh@users.noreply.github.com>
2024-05-13 00:11:44 +0530

    Add files via upload

---
+++ knnstrem.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+def g1():
+    gr1=st.text_input('gene 1')
+    return gr1
+def g2():
+    gr2=st.text_input('gene 2')
+    return gr2
+def predict(gr1, gr2):
+    lmod=pickle.load(open('knn_model.pkl', 'rb'))
+    nd=[[float(gr1), float(gr2)]]
+    pre=lmod.predict(nd)
+    st.write('the cancer is')
+    if pre==0:
+        st.write('not present')
+    else:
+        st.write('present')
+
+if __name__ == "__main__":
+    st.title('cancer presence prediction')
+    st.image('canceer.webp') 
+    g1st = g1()
+    g2nd = g2()
+    st.write("The parameters you entered are: ")
+    st.write("gene1 ", g1st)
+    st.write("gene2", g2nd)
+if st.button('check it'):
+    predict(g1st,g2nd)
